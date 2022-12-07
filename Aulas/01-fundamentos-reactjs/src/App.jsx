5e8e818b0d3e7c4b889e32fa22694b2c101f82cf
Vitor Lopes de Matos <vitorlopesdematos523@gmail.com>
2022-12-06 22:00:20 -0400

    Initial commit

---
+++ Aulas/01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,13 @@
+import { Post } from "./post"
+export function App() {
+  return (
+    <>
+      <Post 
+        author="Vitor Lopes de Matos" 
+        content="Desenvolvedor Front-End Júnior Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus saepe obcaecati placeat omnis quos! Quae, voluptate ipsum! Debitis illum et commodi? Debitis possimus dolore porro praesentium nisi laudantium. Cumque, qui."
+      />
+
+      <Post author="Diego da Rocketseat" content="Educador nota 1000"/>
+    </>
+  )
+}
---
+++ Aulas/01-fundamentos-reactjs/src/post.jsx
@@ -0,0 +1,8 @@
+export function Post({author, content}) {
+  return (
+    <div>
+      <h2>{author}</h2>
+      <p>{content}</p>
+    </div>
+  )
+}
