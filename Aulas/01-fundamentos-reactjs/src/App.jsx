1b499208519d3d7e79b73a20a4b63f03203c7f09
Vitor Lopes de Matos <vitorlopesdematos523@gmail.com>
2022-12-07 20:05:30 -0400

    adicionei a estilização do cabeçalho da aplicação

--- Aulas/01-fundamentos-reactjs/src/App.jsx
+++ Aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,13 +1,29 @@
 import { Post } from "./post"
+import { Header } from "./components/Header"
+
+import styles from './App.module.css'
+import './global.css'
 export function App() {
   return (
-    <>
-      <Post 
-        author="Vitor Lopes de Matos" 
-        content="Desenvolvedor Front-End Júnior Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus saepe obcaecati placeat omnis quos! Quae, voluptate ipsum! Debitis illum et commodi? Debitis possimus dolore porro praesentium nisi laudantium. Cumque, qui."
-      />
+    <div>
+      <Header />
 
-      <Post author="Diego da Rocketseat" content="Educador nota 1000"/>
-    </>
+      <div className={styles.wrapper}>
+        <aside>
+          sidebar
+        </aside>
+        <main>
+          <Post 
+            author="Vitor Lopes" 
+              content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, aut sit odio voluptas facilis eveniet! Ea odit, officiis illo dolore tempore, quibusdam similique suscipit, maxime vitae eum provident aliquam iure."
+          />
+
+          <Post 
+            author="Diego Rocketseat" 
+            content="Um Post muito legal"
+          />
+        </main>
+      </div>
+    </div>
   )
 }
