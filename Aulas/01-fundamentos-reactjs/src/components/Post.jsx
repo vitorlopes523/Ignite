f06799bcdbcb41782d227bc17125fbbe9ad57bfa
Vitor Lopes de Matos <vitorlopesdematos523@gmail.com>
2022-12-28 16:00:13 -0400

    adicionei a estilização do projeto

--- Aulas/01-fundamentos-reactjs/src/components/Post.jsx
+++ Aulas/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,4 +1,5 @@
 import { Comment } from './Comment'
+import { Avatar } from './Avatar'
 import styles from './Post.module.css'
 
 export function Post() {
@@ -6,7 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/vitorlopes523.png" />
+          <Avatar src="https://github.com/vitorlopes523.png" />
           <div className={styles.authorInfo}>
             <strong>Vitor Lopes de Matos</strong>
             <span>Front-end Jr Developer</span>
--- Aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ Aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilLine} from 'phosphor-react'
 
 import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar'
 
 export function Sidebar() {
   return (
@@ -11,7 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/vitorlopes523.png"/>
+        <Avatar src="https://github.com/vitorlopes523.png" />
 
         <strong>Vitor Lopes de Matos</strong>
         <span>Front-end Jr Developer</span>
