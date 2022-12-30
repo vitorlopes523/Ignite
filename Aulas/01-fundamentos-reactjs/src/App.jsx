3809750d3fa187a5c597ada171cec12f87f5bf10
Vitor Lopes de Matos <vitorlopesdematos523@gmail.com>
2022-12-30 15:41:46 -0400

    adicionei o conteúdo das duas primeiras aulas do módulo Os motores do React

--- Aulas/01-fundamentos-reactjs/src/App.jsx
+++ Aulas/01-fundamentos-reactjs/src/App.jsx
@@ -5,6 +5,41 @@ import { Sidebar } from "./components/Sidebar"
 import styles from './App.module.css'
 import './global.css'
 
+//author: { avatar_url: "", role: ""}
+//publishAt: Date
+//content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/vitorlopes523.png',
+      name: 'Vitor Lopes de Matos',
+      role: 'Front-end Jr Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-12-30 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-05-31 20:00:00'),
+  }
+]
+
 export function App() {
   return (
     <div>
@@ -13,15 +48,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author="Vitor Lopes" 
-              content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, aut sit odio voluptas facilis eveniet! Ea odit, officiis illo dolore tempore, quibusdam similique suscipit, maxime vitae eum provident aliquam iure."
-          />
-
-          <Post 
-            author="Diego Rocketseat" 
-            content="Um Post muito legal"
-          />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
--- Aulas/01-fundamentos-reactjs/src/components/Post.jsx
+++ Aulas/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,31 +1,43 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
 import { Comment } from './Comment'
 import { Avatar } from './Avatar'
 import styles from './Post.module.css'
 
-export function Post() {
+export function Post({ author, publishedAt, content }) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", { locale: ptBR})
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/vitorlopes523.png" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Vitor Lopes de Matos</strong>
-            <span>Front-end Jr Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title='26 de Dezembro às 15:00' dateTime="2022-05-11 08:13:30">Publicado há 1h</time>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p>👉{' '}<a href="">jane.design/doctorcare</a></p>
-        <p>
-          <a href="#">#novoprojeto</a>{' '}
-          <a href="#">#nlw</a>{' '}
-          <a href="#">#rocketseat</a>
-        </p>
+        {content.map(line => {
+          if(line.type === 'paragraph') {
+            return <p>{line.content}</p>
+          } else if(line.type === 'link') {
+            return <p>
+                      <a href="#">{line.content}</a>
+                  </p>
+          }
+        })}
       </div>
 
       <form className={styles.comentForm}>
