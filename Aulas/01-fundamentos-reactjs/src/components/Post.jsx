084a4a8b8b7e3ef047deb8eeb168fdc3f8d3cad1
Vitor Lopes de Matos <vitorlopesdematos523@gmail.com>
2023-01-03 15:56:44 -0400

    adicionei conteúdo das aulas do módulo Motores do React

--- Aulas/01-fundamentos-reactjs/src/components/Post.jsx
+++ Aulas/01-fundamentos-reactjs/src/components/Post.jsx
@@ -4,14 +4,33 @@ import ptBR from 'date-fns/locale/pt-BR'
 import { Comment } from './Comment'
 import { Avatar } from './Avatar'
 import styles from './Post.module.css'
+import { useState } from 'react'
 
 export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([
+    'Post muito bacana hein?!'
+  ])
+
+  const [newCommentText, setNewCommentText] = useState('')
+
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", { locale: ptBR})
 
   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
     locale: ptBR,
     addSuffix: true,
   })
+
+  function handleCreateNewComment() {
+    event.preventDefault()
+    
+    setComments([...comments, newCommentText])
+    setNewCommentText('')
+  }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value)
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -31,20 +50,23 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         {content.map(line => {
           if(line.type === 'paragraph') {
-            return <p>{line.content}</p>
+            return <p key={line.content}>{line.content}</p>
           } else if(line.type === 'link') {
-            return <p>
+            return <p key={line.content}>
                       <a href="#">{line.content}</a>
                   </p>
           }
         })}
       </div>
 
-      <form className={styles.comentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.comentForm}>
         <strong>Deixe seu fedback</strong>
 
         <textarea
+          name="comment"
           placeholder='Deixe seu comentário'
+          value={newCommentText}
+          onChange={handleNewCommentChange}
         />
 
         <footer>
@@ -53,9 +75,9 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return <Comment key={content} content={comment} />
+        })}
       </div>
     </article>
   )
