677d7c45ca2810cbdabf2df0e576f8adc06675d1
Vitor Lopes de Matos <vitorlopesdematos523@gmail.com>
2023-01-04 07:44:11 -0400

    finalizei o módulo Os Motores do React

--- Aulas/01-fundamentos-reactjs/src/components/Comment.jsx
+++ Aulas/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,11 +1,19 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
+import { useState } from 'react'
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0)
 
   function handleDeleteComment() {
     onDeleteComment(content)
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    })
   }
 
   return (
@@ -29,9 +37,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
