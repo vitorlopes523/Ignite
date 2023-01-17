61574acdb51f00df2a75dcdf0d59e90e2f339741
Vitor Lopes de Matos <vitorlopesdematos523@gmail.com>
2023-01-17 17:12:52 -0400

    adicionei as estilizações globais da aplicação e iniciei a criação de rotas

--- Aulas/02-ignite-timer/src/App.tsx
+++ Aulas/02-ignite-timer/src/App.tsx
@@ -1,6 +1,21 @@
+import { ThemeProvider} from "styled-components"
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from "./Router"
+
+import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from "./styles/themes/default"
+
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+
+        <Router />
+        
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
