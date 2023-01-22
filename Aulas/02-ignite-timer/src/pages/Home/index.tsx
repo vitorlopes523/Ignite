7581f6536f97109a7377ad44e2edadd550eec6d3
Vitor Lopes de Matos <vitorlopesdematos523@gmail.com>
2023-01-22 10:44:01 -0400

    Adicionei a estilização do formulário, com styled components

--- Aulas/02-ignite-timer/src/pages/Home/index.tsx
+++ Aulas/02-ignite-timer/src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { Play } from 'phosphor-react'
-import { CountdownContainer, FormContainer, HomeContainer, Separator } from './styles'
+import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from './styles'
 
 export function Home() {
   return (
@@ -7,9 +7,16 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
-          <label htmlFor="minuteAmount">durante</label>
-          <input type="number" id="minuteAmount" />
+          <TaskInput id="task" list='task-suggestions' placeholder='Dê um nome para o seu projeto' />
+
+          <datalist id='task-suggestions'>
+            <option value="Projeto 1"/>
+            <option value="Projeto 2"/>
+            <option value="Projeto 3"/>
+          </datalist>
+
+          <label htmlFor="minuteAmount" >durante</label>
+          <MinutesAmountInput type="number" id="minuteAmount" placeholder='00' step={5} min={5} max={60} />
           <span>minutos.</span>
         </FormContainer>
 
@@ -21,10 +28,10 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <button type="submit">
+        <StartCountdownButton disabled type="submit">
           <Play size={24} />
           Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   )
